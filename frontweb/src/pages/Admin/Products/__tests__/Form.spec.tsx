d7d6958029ffe0e901420a1bd096281d4602673f
Silvio Limeira <sicemal@gmail.com>
2023-04-04 19:13:01 -0400

    test should clear validation messages when filling out the form correctly

--- frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -81,8 +81,52 @@ describe('Product form create tests', () => {
 
         })
 
+        const nameInput = screen.getByTestId("name")
+        const priceInput = screen.getByTestId("price")
+        const imgUrlInput = screen.getByTestId("imgUrl")
+        const descriptionInput = screen.getByTestId("description")
+        const categoriesInput = screen.getByLabelText("Categorias")
+
+        await selectEvent.select(categoriesInput, ['Computadores', 'Livros'])
+        userEvent.type(nameInput, "Computador")
+        userEvent.type(priceInput, "5000.12")
+        userEvent.type(imgUrlInput, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/23-big.jpg");
+        userEvent.type(descriptionInput, "Computador muito bom")
+
+        await waitFor(() => {
+            const messages = screen.queryAllByText('Campo obrigatório')
+            expect(messages).toHaveLength(0);
+        })
+
 
     })
+
+
+    test('Should clear validation messages when filling out the form correctly', async () => {
+
+        render(
+            <Router history={history}>
+                <Form />
+            </Router>
+        )
+
+        const submitButton = screen.getByRole('button', { name: /salvar/i })
+
+        userEvent.click(submitButton)
+
+        await waitFor(() => {
+
+            const messages = screen.getAllByText('Campo obrigatório')
+
+            expect(messages).toHaveLength(5);
+
+        })
+
+
+
+
+    })
+
 
 })
 
